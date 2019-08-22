7e296deae425d062839151c80faf8a5889db022b
kangmooo <kangmooo@hotmail.com>
2019-08-22 23:32:29 +0900

    java version 12 로 변경 autowired 설정 추가

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -3,23 +3,25 @@ package com.example.demo.controller;
 import com.example.demo.entity.TestTable;
 import com.example.demo.repository.DemoQueryDslRepository;
 import com.example.demo.repository.DemoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+//@RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/demo")
 public class DemoController {
 
-    private final DemoQueryDslRepository demoQueryDslRepository;
-    private final DemoRepository demoRepository;
+    @Autowired private DemoQueryDslRepository demoQueryDslRepository;
+    @Autowired private DemoRepository demoRepository;
 
-    public DemoController(DemoQueryDslRepository demoQueryDslRepository, DemoRepository demoRepository) {
-        this.demoQueryDslRepository = demoQueryDslRepository;
-        this.demoRepository = demoRepository;
-    }
+//    public DemoController(DemoQueryDslRepository demoQueryDslRepository, DemoRepository demoRepository) {
+//        this.demoQueryDslRepository = demoQueryDslRepository;
+//        this.demoRepository = demoRepository;
+//    }
 
     @GetMapping("/test")
     public String test(){
